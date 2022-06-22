9f614bd53079d6bb368c66afb039076eddd93ba1
Robert Amour <robert@ferretech.io>
2022-06-21 17:27:33 -0700

    Add scale exercise generation

--- api/views.py
+++ api/views.py
@@ -44,8 +44,63 @@ def get_all_chromatic_notes(request):
         {'notes': [n.unicodeNameWithOctave for n in scale.ChromaticScale('C').getPitches('A0', 'C8')]})
 
 
-def generate_scale(scale: str) -> JsonResponse:
-    scale.Scale
+def generate_scale(request) -> JsonResponse:
+    tonic = request.GET.get('tonic', 'ab')
+    quality = request.GET.get('quality', 'melodic')
+    print(f'Generating {tonic} {quality} scale...')
 
+    if quality == 'major':
+        k = key.Key(tonic.upper())
+        scl = k.getScale('major')
+    elif quality == 'minor':
+        k = key.Key(tonic.lower())
+        scl = k.getScale('minor')
+    elif quality == 'melodic':
+        k = key.Key(tonic.lower())
+        scl = scale.MelodicMinorScale(tonic)
+    elif quality == 'harmonic':
+        k = key.Key(tonic.lower())
+        scl = scale.HarmonicMinorScale(tonic)
+    else:
+        raise ('Invalid key quality passed to generate_scale:', quality)
 
-# Create your views here.
+    left_hand, right_hand, grand_staff, s = create_grand_staff(key.KeySignature(k.sharps))
+    time_sig = meter.TimeSignature('7/4')
+    time_sig.beamSequence.partition(7)
+    left_hand.insert(time_sig)
+    right_hand.insert(time_sig)
+    sixteenth = duration.Duration(0.25)
+
+    # Spell scale
+    bottom_note = scl.pitchFromDegree(1)
+    if bottom_note >= pitch.Pitch('F4'):
+        bottom_note = bottom_note.transpose('-p8')
+    top_note = bottom_note.transpose('p8').transpose('p8')
+    asc = scale.DIRECTION_ASCENDING
+    desc = scale.DIRECTION_DESCENDING
+    rh_notes = [note.Note(p, duration=sixteenth) for p in scl.getPitches(bottom_note, top_note, asc)]
+    rh_notes.extend(
+        [note.Note(p, duration=sixteenth) for p in scl.getPitches(bottom_note, top_note, desc)][1:])
+    rh_notes[-1].duration = duration.Duration(1)
+    lh_notes = [n.transpose('-p8') for n in rh_notes]
+    right_hand.append(rh_notes)
+    left_hand.append(lh_notes)
+    p = bottom_note
+
+    new_clef_threshold_asc = pitch.Pitch('F4')
+    new_clef_threshold_desc = pitch.Pitch('Bb3')
+    inserted_treble = False
+    for n in left_hand.notes:
+        if not inserted_treble and n.pitch > new_clef_threshold_asc:
+            left_hand.insert(int(n.offset), clef.TrebleClef())
+            inserted_treble = True
+        if inserted_treble and n.pitch < new_clef_threshold_desc:
+            left_hand.insert(int(n.offset), clef.BassClef())
+            break
+
+    for part in left_hand, right_hand:
+        part.makeNotation(inPlace=True)  # makes measures
+
+    parser = musicxml.m21ToXml.GeneralObjectExporter(s)
+    print('Done generating scale!')
+    return JsonResponse({'xml': parser.parse().decode('utf-8')})

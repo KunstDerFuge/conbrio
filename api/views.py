e10807047ff17529eb16d375e8add5cb4117841b
Robert Amour <robert@ferretech.io>
2022-06-21 17:27:13 -0700

    Refactor random note exercise code

--- api/views.py
+++ api/views.py
@@ -13,21 +13,12 @@ def get_random_note(request):
     max_note = replace_fancy_accidentals(request.GET.get('max_note', 'C8'))
     accidentals = request.GET.get('accidentals', 'true') == 'true'
 
-    # Create grand staff
-    left_hand = stream.PartStaff()
-    right_hand = stream.PartStaff()
-    grand_staff = layout.StaffGroup([right_hand, left_hand])
-    s = stream.Score([right_hand, left_hand, grand_staff])
-
     # Choose random key signature, hand, and note
-    key_sig = random.randint(max_flats * -1, max_sharps)
+    key_sig = key.KeySignature(random.randint(max_flats * -1, max_sharps))
     hand = random.randint(0, 1)
-    random_note = generate_random_note(min_note, max_note, accidentals, key.KeySignature(key_sig))
+    left_hand, right_hand, grand_staff, s = create_grand_staff(key_sig)
 
-    left_hand.insert(clef.BassClef())
-    right_hand.insert(clef.TrebleClef())
-    left_hand.insert(0, key.KeySignature(key_sig))
-    right_hand.insert(0, key.KeySignature(key_sig))
+    random_note = generate_random_note(min_note, max_note, accidentals, key_sig)
 
     print(random_note)
 

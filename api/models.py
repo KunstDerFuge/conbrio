096082bee1eebd1903c1354bb47effc22f390a8c
Robert Amour <robert@ferretech.io>
2023-07-18 14:43:50 -0700

    Use foreign key for scores, not many to many

--- api/models.py
+++ api/models.py
@@ -27,7 +27,7 @@ class ExerciseScore(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    exercise_scores = models.ManyToManyField(Exercise, through='ExerciseScore')
+    exercise_scores = models.ForeignKey(ExerciseScore, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.user) + '\'s profile'

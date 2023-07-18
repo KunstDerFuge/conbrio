6256cca5439ecd9f724a9fda2d8ca7c23e05f404
Robert Amour <robert@ferretech.io>
2023-07-18 02:19:51 -0700

    Define some basic models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Exercise(models.Model):
+    pass
+
+
+class Scale(Exercise):
+    scale = models.CharField(6)
+
+
+class Arpeggio(Exercise):
+    chord = models.CharField(6)
+
+
+class ChordExercise(Exercise):
+    chord = models.CharField(6)
+
+
+class ExerciseScore(models.Model):
+    accuracy = models.FloatField()
+    notes_per_minute = models.FloatField()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    exercise_scores = models.ManyToManyField(Exercise, through='ExerciseScore')
+
+    def __str__(self):
+        return str(self.user) + '\'s profile'
+
+    def get_score(self):
+        pass
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
